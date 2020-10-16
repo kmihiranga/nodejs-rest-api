4e962547f146f9390d853750cb03c855255d1dcd
kalana <kalana.mihiranga@hotmail.com>
2020-10-16 18:00:27 +0530

    creating routes and responses

--- server.js
+++ server.js
@@ -6,8 +6,34 @@ dotenv.config({path: './config/config.env'});
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from express');
+// get all bootcamps
+app.get('/api/v1/bootcamps', (req, res) => {
+  // res.send('Hello from express');
+  res.status(200).json({success: true, msg: 'Show all bootcamps'});
+});
+
+// get single bootcamps
+app.get('api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({success: true, msg: `show bootcamp ${req.params.id}`});
+});
+
+// save a bootcamp
+app.post('api/v1/bootcamps', (req, res) => {
+  res.status(200).json({success: true, msg: 'Create new bootcamp'});
+});
+
+// update bootcamp
+app.put('api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({success: true, msg: `Update bootcamp ${req.params.id}`});
+});
+
+// delete bootcamp
+app.delete('api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({success: true, msg: `Delete bootcamp ${req.params.id}`});
 });
 
 const PORT = process.env.PORT || 5000;
